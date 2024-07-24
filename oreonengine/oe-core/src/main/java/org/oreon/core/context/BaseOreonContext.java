8ed09b46010d695d4a1beedd45c8d888841392db
Aliaksei Piasetski <aleksey.pesetski@gmail.com>
2024-07-24 17:39:21 +0200

    update versions and rework context

---
+++ oreonengine/oe-core/src/main/java/org/oreon/core/context/BaseOreonContext.java
@@ -0,0 +1,34 @@
+package org.oreon.core.context;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.oreon.core.CoreEngine;
+import org.oreon.core.RenderEngine;
+import org.oreon.core.platform.GLFWInput;
+import org.oreon.core.platform.Window;
+import org.oreon.core.scenegraph.Camera;
+
+@Getter
+public class BaseOreonContext<C extends Camera, W extends Window> implements OreonContext<C, W> {
+
+  private Config config;
+  private GLFWInput input;
+  @Setter
+  private RenderEngine renderEngine;
+  @Setter
+  private CoreEngine coreEngine;
+
+  private C camera;
+  private W window;
+
+  protected BaseOreonContext(final C camera, final W window, final RenderEngine renderEngine) {
+    this.camera = camera;
+    this.window = window;
+    this.coreEngine = new CoreEngine();
+
+    this.config = new Config();
+    this.input = new GLFWInput();
+
+    this.renderEngine = renderEngine;
+  }
+}
